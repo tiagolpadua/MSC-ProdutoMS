466f098c397188ad2913b76542b1fded2df2b52a
tiago <tiagolpadua@gmail.com>
2019-01-02 23:28:52 -0200

    Commit inicial

---
+++ src/main/java/com/example/ProdutoMS/ProdutoMsApplication.java
@@ -0,0 +1,14 @@
+package com.example.ProdutoMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoMsApplication.class, args);
+	}
+
+}
+
